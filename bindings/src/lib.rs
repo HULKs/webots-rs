3e90616064f9eb84c3be5abe7098e7ad20e91511
Hendrik <git@h3ndrk.de>
2021-12-07 08:39:14 +0100

    Add bindings sub-crate

--- bindings/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![allow(clippy::all)]
-#![allow(improper_ctypes)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-include!(concat!(env!("OUT_DIR"), "/webots_bindings.rs"));
